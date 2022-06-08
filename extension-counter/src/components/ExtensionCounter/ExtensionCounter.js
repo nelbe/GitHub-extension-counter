6a505cd5410b6bd4e195fc796c96300b0d560992
Belen <belen.dominguez@sectoralarm.eu>
2022-06-08 23:22:55 +0200

    First structure

---
+++ extension-counter/src/components/ExtensionCounter/ExtensionCounter.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { form } from './ExtensionCounter.style';
+
+const ExtensionCounter = () => {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default ExtensionCounter;
